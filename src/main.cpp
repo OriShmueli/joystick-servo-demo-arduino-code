1bd315fa3f116dfb0881f929274b54841a118650
Orishmueli <43569659+OriShmueli@users.noreply.github.com>
2023-06-24 23:44:40 +0300

    functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo Base_Servo;
+int baseServoStartingPostion = 0;
+int baseServoPin = 9;
+
+void basicCharSending(const char* message);
+void establishConnectionWithComputer();
 
 void setup() {
-  
+    Serial.begin(1000000);
+    Base_Servo.attach(baseServoPin);
 }
 
 void loop() {
-  
+  while (Serial.available() > 0)    
+  {
+    
+  }
+}
+
+void basicCharSending(const char* message){
+
+}
+
+void establishConnectionWithComputer(){
+
 }

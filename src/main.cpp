1f97c3a7a1eab687d656459c69cadddda6e76845
Orishmueli <43569659+OriShmueli@users.noreply.github.com>
2023-06-25 16:15:08 +0300

    new code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+#define VRX_PIN A0;
+#define VRY_PIN A1;
+
 Servo Base_Servo;
 int baseServoStartingPostion = 0;
 int baseServoPin = 9;
 
 void basicCharSending(const char* message);
 void establishConnectionWithComputer();
+bool connectedToComputer = false;
+const unsigned long eventInterval = 1000;
+unsigned long previousTime = 0;
+
+int xValue = 0;
+int yValue = 0;
+
+int PrevValueX = 0;
+int PrevValueY = 0;
 
 void setup() {
     Serial.begin(1000000);
@@ -14,14 +26,30 @@ void setup() {
 }
 
 void loop() {
+  unsigned long currentTime = millis();
+  xValue = analogRead(VRX_PIN);
+  yValue = analogRead(VRY_PIN);
+
   while (Serial.available() > 0)    
   {
-    
+    if(!establishConnectionWithComputer){
+      establishConnectionWithComputer()
+    }else{
+      char getByte = Serial.read();
+      if(getByte == 'c'){
+        
+      }
+    }
+    /*if(currentTime - previousTime >= eventInterval){
+      previousTime = currentTime;
+    }*/
+   
   }
 }
 
 void basicCharSending(const char* message){
-
+  char getByte = Serial.read();
+  if(getbyte == '')
 }
 
 void establishConnectionWithComputer(){
